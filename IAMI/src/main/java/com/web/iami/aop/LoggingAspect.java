b7e207a3606294401a9b22e179325b56583329f2
jin_a <jin_a@DESKTOP-O1DFB21>
2019-05-04 02:37:57 +0900

    AOP 로그 설정

---
+++ IAMI/src/main/java/com/web/iami/aop/LoggingAspect.java
@@ -0,0 +1,58 @@
+package com.web.iami.aop;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Component
+@Aspect
+public class LoggingAspect {
+
+	private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+	
+	@Around("execution(* com.web.iami.controller.*.*(..))")
+	public Object requestLogging(ProceedingJoinPoint jp) throws Throwable {
+		
+		logger.info("----------------------------------------------------------------------------------------------");
+	
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+		
+		String signature = jp.getSignature().getName();
+		logger.info("요청주소 : " + request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + request.getRequestURI());
+		logger.info("메소드명 : " + signature);
+		logger.info("파라미터 : " + Arrays.toString(jp.getArgs()));
+		long startTime = System.currentTimeMillis();
+		Calendar currCal = Calendar.getInstance();
+		
+		try {
+			Object obj = jp.proceed();
+			return obj;
+		} finally {
+			long endTime = System.currentTimeMillis();
+		
+	        String ip = request.getHeader("X-FORWARDED-FOR");
+	        if (ip == null) {
+	            ip = request.getRemoteAddr();
+	        }
+			
+			logger.info("접속유저 : " + ip);
+			logger.info("실행시간 : " + (endTime - startTime) + "ms");
+			logger.info("실행일시 : " + new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(currCal.getTime()));
+			
+			logger.info("----------------------------------------------------------------------------------------------");
+		}
+
+	}
+
+}
--- IAMI/src/main/webapp/WEB-INF/views/main.jsp
+++ IAMI/src/main/webapp/WEB-INF/views/main.jsp
@@ -6,6 +6,20 @@
 <meta charset="UTF-8">
 <title>IAMI</title>
 <jsp:include page="/resources/common/common.jsp"/>
+<script type="text/javascript">
+$(document).ready(function() {
+	resize();
+});
+$(window).resize(resize);
+function resize() {
+	$htmlHeight = $("html").height();
+	$headerHeight = $(".headerWrapper").height();
+	$navHeight = $(".navWrapper").height();
+	$footerHeight = $(".footerWrapper").height();
+	
+	$(".container-fluid").css("min-height", $htmlHeight - $headerHeight - $navHeight - $footerHeight - 42 + "px");
+}
+</script>
 <style type="text/css">
 .headerWrapper {
 	border-bottom: 1px solid #D5D5D5;
@@ -14,6 +28,38 @@
 	max-width: 1200px;
 	margin: auto;
 	padding: 0;
+}
+.navWrapper {
+	border-bottom: 1px solid #D5D5D5;
+	background-color: rgba(17, 135, 207, 0.4);
+}
+.navWrapper .navInner {
+	width: 1200px;
+	margin: auto;
+	color: white;
+}
+.navWrapper .navInner ul {
+	list-style-type: none;
+	margin: 0;
+	padding: 0;
+	overflow: hidden;
+}
+.navWrapper .navInner ul li {
+	display: inline-block;
+	padding: 10px;
+	cursor: pointer;
+}
+.navWrapper .navInner ul li span:hover {
+	border-bottom: 3px solid white;
+}
+.footerWrapper {
+	padding: 20px;
+	font-size: 10pt;
+	text-align: center;
+	background-color: #f4f5f7;
+}
+.footerWrapper span {
+	color: gray;
 }
 </style>
 </head>
@@ -25,11 +71,21 @@
 		</div>
 	</div>
 </header>
+<nav class="navWrapper">
+	<div class="navInner">
+		<ul>
+			<li><span>전체</span></li>
+			<li><span>친구</span></li>
+		</ul>
+	</div>
+</nav>
 <section class="contentWrapper">
-테스트
+	<div class="container-fluid">
+		컨테이너
+	</div>
 </section>
 <footer class="footerWrapper">
-푸터
+Copyright <span>KimJongHyeok.</span> All Rights Reserved.
 </footer>
 </body>
 </html>
